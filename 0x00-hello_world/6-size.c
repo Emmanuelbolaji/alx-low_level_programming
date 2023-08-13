780a9216e086824fea89a23c3599e0e31ca49cb8
Emmanuelbolaji <osundiranemmanuel@gmail.comgit config --global user.email osundiranemmanuel@gmail.com>
2023-08-13 10:57:48 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	printf("size of a char: 1 byte(s)\n");
+	printf("size of an int: 4 byte(s)\n");
+	printf("size of a long int: 4 byte(s)\n");
+	printf("size of a long long int: 8 bytes(s)\n");
+	printf("size of a float: 4 byte(s)\n");
+	return (0);
+}
