d1ecf48b2090b867bc5f32c2ba675994ad79af00
Emmanuelbolaji <osundiranemmanuel@gmail.comgit config --global user.email osundiranemmanuel@gmail.com>
2023-08-16 09:55:55 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main - Print all the alphabet from a to z in lower case
- * Return: always void
+ * print_alphabet - Prints all the lowercase alphabets
+ *
+ * Return: Always 0
  */
-
 void print_alphabet(void)
 {
 	char let;
-	for(let = 'a'; let <= 'z'; let++)
+
+	for (let = 'a'; let <= 'z'; let++)
 	{
-		_putchar("%c", let);
+		_putchar(let);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
