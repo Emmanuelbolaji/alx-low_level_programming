2e9cde51d18e9eafb2b64c80a68cc7092f4765a3
Emmanuelbolaji <osundiranemmanuel@gmail.comgit config --global user.email osundiranemmanuel@gmail.com>
2023-08-15 18:03:35 -0700

    main prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <main.h>
+
+/**
+ * print_alphabets - Prints the alphabets from a to z in lower case
+ * return void
+ */
+void print_alphabets(void)
+{
+    char let;
+
+    for (let = 'a'; let <= 'z'; let++)
+    {
+        _putchar(letter);
+
+    }
+
+    _putchar('\n');
+}
+
+int main(void)
+{
+    print_alphabets();
+    return (0);
+}
+
