0db4a0767db504a549c9f61b2a14104614d43b52
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-09-05 09:18:11 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    if (argc > 0) {
-        printf("%s\n", argv[0]);
-    }
-    return 0;
-}
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: number of arguements
+ * @argv: arguements vector
+ */
 
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
