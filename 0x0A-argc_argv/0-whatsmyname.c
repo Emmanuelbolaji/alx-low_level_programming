838d009438ef9dbc1e001b30b6e88a8277ac956b
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-09-05 09:24:22 -0700

    0-whatsmynamea.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,7 @@
  * main - a program that prints its name, followed by a new line
  * @argc: number of arguements
  * @argv: arguements vector
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
