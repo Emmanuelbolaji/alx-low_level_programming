74d396d925c98cc298569ea2c5cf669855230bd0
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-09-08 16:47:52 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,38 +2,60 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int is_valid_input(const char *input) {
-    while (*input) {
-        if (!isdigit(*input)) {
-            return 0;
-        }
-        input++;
-    }
-    return 1;
+/**
+ * is_valid_in - function name
+ * @input: arguement
+ * Return: number
+ */
+
+int is_valid_in(const char *in)
+{
+while (*in)
+{
+if (!isdigit(*in))
+{
+return (0);
+}
+in++;
+}
+return (1);
 }
 
-int multiply(int num1, int num2) {
-    return num1 * num2;
+/**
+ * mul - function
+ * Return: number
+ */
+
+int mul(int num1, int num2)
+{
+return (num1 * num2);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Error\n");
-        return 98;
-    }
+/**
+ * main - function name
+ * @argc: arguement
+ * @argv: arguement
+ * Return: 0 on success 98 on failure
+ */
 
-    const char *num1_str = argv[1];
-    const char *num2_str = argv[2];
+int main(int argc, char *argv[])
+{
+const char *num1_str = argv[1];
+const char *num2_str = argv[2];
+int num1 = atoi(num1_str);
+int num2 = atoi(num2_str);
+int result = mul(num1, num2);
 
-    if (!is_valid_input(num1_str) || !is_valid_input(num2_str)) {
-        printf("Error\n");
-        return 98;
-    }
-
-    int num1 = atoi(num1_str);
-    int num2 = atoi(num2_str);
-
-    int result = multiply(num1, num2);
-    printf("%d\n", result);
+if (argc != 3)
+{
+printf("Error\n");
+return (98);
+}
+if (is_valid_in(num1_str) || !is_valid_in(num2_str))
+{
+printf("Error\n");
+return (98);
+}
+printf("%d\n", result);
 return (0);
 }
