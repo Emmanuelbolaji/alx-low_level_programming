f382861ba8665877f31393ebb5c781fa1e2874a2
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-09-11 09:35:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name of puppy
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: alx c structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
