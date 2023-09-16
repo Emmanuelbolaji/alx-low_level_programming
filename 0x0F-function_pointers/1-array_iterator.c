1f148009eadae852f7209bcf41accff079ab494c
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-09-15 17:16:03 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,28 @@
-dd
+#include <stdio.h>
+
+/**
+ * array_iterator -  a function that executes a function
+ * @array: arguement`
+ * @size: arguement 1
+ * @action: arguement 2
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a;
+
+	for (a = 0; a < size; a++)
+	{
+		action(array[a]);
+	}
+}
+
+/**
+ * print_element - function arguement
+ * @element: arguement 1
+ */
+
+void print_element(int element)
+{
+	printf("%d ", element);
+}
