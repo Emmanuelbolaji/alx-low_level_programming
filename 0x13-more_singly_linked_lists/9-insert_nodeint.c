6535817a45aee6bd26471b4e86d270554e8320b4
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-09-26 05:45:43 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,25 @@
 #include "lists.h"
 
 /**
+ * make_new_node - creates new node
+ * @n: data of new node
+ * Return: pointer to node
+ */
+
+listint_t *make_new_node(int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
  * insert_nodeint_at_index - Inserts new node at given position in linked list.
  * @head: pointer to a pointer to the head of the linked list.
  * @idx: The index at which the new node should be added.
@@ -10,34 +29,40 @@
  *
  * Return: The address of the new node, or NULL if it failed.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *t;
 	unsigned int a;
+	listint_t *tp;
+	listint_t *tp_old;
+	listint_t *new_node;
 
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	tp = *head;
+	if (head == NULL)
 		return (NULL);
-
-	new_node->n = n;
-
-	if (idx == 0 || *head == NULL)
+	new_node = make_new_node(n);
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
 	{
-		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	t = *head;
-	for (a = 0; a < idx - 1 && t != NULL; a++)
+
+	if (idx == 0)
+		*head = new_node;
+	for (a = 0; a < idx - 1 && tp != NULL && idx != 0; a++)
+		tp = tp->next;
+	if (tp == NULL)
+		return (NULL);
+	if (idx == 0)
+		new_node->next = tp;
+	else
 	{
-		t = t->next;
+		tp_old = tp->next;
+		tp->next = new_node;
+		new_node->next = tp_old;
 	}
-	if (t == NULL)
-		free(new_node);
-	return (NULL);
-
-	new_node->next = t->next;
-	t->next = new_node;
-
 	return (new_node);
 }
+
