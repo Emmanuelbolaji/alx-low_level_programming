767d871c2f2cf43a344f5c1120b2a43f0d3177f6
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-11-06 08:02:58 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
